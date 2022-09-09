3082225cf6f9fc3459051d87d6a588c5c2612d6d
otanisae <otani-sae-bg@ynu.jp>
2022-09-09 16:51:57 +0900

    ✨ 背景の追加

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/events">Events</router-link>
   </nav>
-  <router-view/>
+  <router-view class="router-view"/>
+  <Background class="background"></Background>
 </template>
 
 <style>
@@ -27,4 +28,28 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+
+
+.router-view{
+  width: calc(100vw - 1rem);
+  margin: 0 auto;
+}
+
+.background{
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background-color: black;
+  top: 0;
+  z-index: -100;
+}
+
+
 </style>
+<script>
+import Background from "@/components/BackgroundComponent";
+export default {
+  components: {Background}
+}
+</script>
