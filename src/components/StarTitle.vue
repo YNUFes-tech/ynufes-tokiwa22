ef7894bf579d09633708bcaabc4ac59b6e812c57
Shion Ichikawa <shion1305@gmail.com>
2022-09-24 01:33:22 +0900

    ✨ StarTitleを実装。

---
+++ src/components/StarTitle.vue
@@ -0,0 +1,101 @@
+<script setup>
+import {onMounted} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+onMounted(() => {
+  let targets = document.querySelectorAll('.star-animation-title')
+  window.addEventListener('scroll', function () {
+    var scroll = window.scrollY;
+    var h = window.innerHeight;
+    for (let target of targets) {
+      var pos = target.getBoundingClientRect().top + scroll;
+      if (scroll + h + 100 > pos && scroll - 100 < pos) {//スクロール量 > アニメーションさせたい要素の位置
+        target.classList.add('star-animation');
+      } else {
+        target.classList.remove('star-animation')
+      }
+    }
+  })
+})
+</script>
+<template>
+  <div class="star-animation-title star-animation">
+    <div class="star-animation-title-text">{{ props.title }}</div>
+    <div class="star-animation-title-tail"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.star-animation-title-tail {
+  width: 100%;
+  background: radial-gradient(ellipse at right, #cfd9e5 0%, #44444455 80%, #11111100 100%);
+  height: 5px;
+  padding: 2px 0;
+  position: relative;
+  opacity: 0;
+}
+
+.star-animation-title-text{
+  padding: 0 0.5em;
+}
+
+
+.star-animation {
+  .star-animation-title-tail {
+    animation: comet 1s forwards;
+    animation-delay: 0.3s;
+
+    &::before {
+      animation-delay: 0.3s;
+      animation: star-rotate 1s forwards;
+    }
+  }
+}
+
+.star-animation-title-tail::before {
+  content: "";
+  vertical-align: middle;
+  background-size: cover;
+  width: 15px;
+  height: 15px;
+  background: url("@/assets/star-white.webp") left center no-repeat;
+  position: absolute;
+  right: -5px;
+  top: -4px;
+  background-size: 15px 15px;
+}
+
+.star-animation-title {
+  color: white;
+  width: fit-content;
+  font-size: 2em;
+}
+
+@keyframes comet {
+  from {
+    opacity: 0;
+    height: 0;
+    width: 0;
+  }
+  to {
+    opacity: 1;
+    height: 5px;
+    width: 100%;
+  }
+}
+
+@keyframes star-rotate {
+  from {
+    transform: rotate(-90deg);
+  }
+  to {
+    transform: rotate(0);
+  }
+}
+
+</style>
