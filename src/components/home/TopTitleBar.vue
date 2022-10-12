37792e7e650e3740840c6c131e4f6e66a8f8c419
Shion Ichikawa <shion1305@gmail.com>
2022-10-12 16:34:11 +0900

    ✨ TopTitleBar.vueの追加

---
+++ src/components/home/TopTitleBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="top-title-bar">
+    <div>
+      <div class="title">星彩</div>
+      <div class="date">2022.9.28(金)~30(日)<br>@横浜国立大学<br>事前予約制</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+#top-title-bar {
+  width: 100vw;
+  position: relative;
+  height: 600px;
+  margin: 0 auto;
+  justify-self: center;
+
+  > div {
+    display: flex;
+    max-width: 400px;
+    flex-direction: column;
+    margin: auto;
+    .title {
+      font-size: 100px;
+    }
+
+    .date {
+      padding-top: 10px;
+      text-align: end;
+      margin: 0 3em;
+    }
+  }
+}
+</style>
