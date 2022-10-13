77e4a77d6e6d7c95b6cd7d5c87d8106e80bd1220
Shion Ichikawa <shion1305@gmail.com>
2022-10-13 19:16:43 +0900

    💄 TopTitleBarのレスポンシブ対応

--- src/components/home/TopTitleBar.vue
+++ src/components/home/TopTitleBar.vue
@@ -2,7 +2,13 @@
   <div id="top-title-bar">
     <div>
       <div class="title">星彩</div>
-      <div class="date">2022.9.28(金)~30(日)<br>@横浜国立大学<br>事前予約制</div>
+      <div class="date">
+        <p class="date-text">
+          事前予約制
+          <br>
+          2022.9.28(金)~30(日)<br>@横浜国立大学
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -12,15 +18,16 @@
 #top-title-bar {
   width: 100vw;
   position: relative;
+  display: flex;
   padding-bottom: 30px;
   margin: 0 auto;
   justify-self: center;
 
   > div {
     display: flex;
-    max-width: 400px;
     flex-direction: column;
     margin: auto;
+
     .title {
       font-size: 100px;
     }
@@ -29,7 +36,41 @@
       padding-top: 10px;
       text-align: end;
       margin: 0 3em;
+      font-size: 20px;
     }
   }
 }
+
+@media screen and (min-width: 768px) {
+  #top-title-bar {
+    height: 400px;
+    align-items: center;
+    justify-content: center;
+
+    > div {
+      flex-direction: row;
+      width: 100%;
+      align-items: center;
+
+      .title {
+        width: 50%;
+        margin: 0;
+        text-align: right;
+        font-size: min(150px, 20vw);
+      }
+
+      .date {
+        width: 50%;
+        margin: 0;
+        align-self: flex-end;
+        display: flex;
+        font-size: 24px;
+        justify-content: center;
+      }
+      .date-text{
+      }
+    }
+  }
+}
+
 </style>
