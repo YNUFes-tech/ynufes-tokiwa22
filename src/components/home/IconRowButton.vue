cb9c7996bbd5755a7ed069c2f913e0bb33d76a39
Shion Ichikawa <shion1305@gmail.com>
2022-10-14 13:24:45 +0900

    ✨ IconRowButton.vueの追加

---
+++ src/components/home/IconRowButton.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+const props = defineProps({
+  iconPath: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    default: '未指定'
+  }
+})
+</script>
+<template>
+  <div class="home-button">
+    <img class="icon" :src="props.iconPath">
+    <div class="button-title" v-html="props.title"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-button {
+  width: 15em;
+  display: flex;
+  align-items: center;
+  background: #42b983;
+  border-radius: 10px;
+  height: 40px;
+  padding: 0.3em;
+
+  .button-title {
+    color: white;
+    margin: auto;
+  }
+
+  .icon {
+    height: 30px;
+    object-fit: cover;
+    padding-right: 0.3em;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .home-button {
+    display: flex;
+    aspect-ratio: 1;
+    height: auto;
+    width: 10em;
+    flex-direction: column;
+    .icon{
+      width: 70%;
+      height: auto;
+    }
+  }
+}
+</style>
