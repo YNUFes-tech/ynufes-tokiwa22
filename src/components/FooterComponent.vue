19204312323e3162976031d952f79c0cba86b199
Shion Ichikawa <shion1305@gmail.com>
2022-10-14 13:43:17 +0900

    ✨ フッターの作成

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer-frame">
+    <div class="bottom"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.footer-frame {
+  width: 100%;
+  background: #2c3e50;
+  height: 200px;
+
+  .bottom{
+    height: 30px;
+    background: red;
+    width: 100%;
+    bottom: 0;
+  }
+}
+</style>
