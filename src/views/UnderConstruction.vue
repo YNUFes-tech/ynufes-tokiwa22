2cb130b63f1b5350b199b23fc98cfef3a9d8f21b
Shion Ichikawa <shion1305@gmail.com>
2022-10-14 13:42:28 +0900

    ✨ 作成中ページ

---
+++ src/views/UnderConstruction.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    このページは準備中です!<br>公開までしばらくお待ちください
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UnderConstruction"
+}
+</script>
+
+<style scoped>
+
+</style>
