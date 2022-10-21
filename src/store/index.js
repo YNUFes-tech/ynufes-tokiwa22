7250eff03a3529bdfbe183662b197b7929f9a959
Shion Ichikawa <shion1305@gmail.com>
2022-10-21 23:04:25 +0900

    🍱 loading画像の移動

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import {createStore} from 'vuex'
 
 export default createStore({
   state: {
-    sponsors: [{image: {url: "/image/loading1.png"}}, {image: {url: "/assets/loading1.png"}},{image: {url: "/assets/loading1.png"}}],
+    sponsors: [{image: {url: "/data/images/loading1.png"}}, {image: {url: "/data/images/loading1.png"}}, {image: {url: "/data/images/loading1.png"}}, {image: {url: "/data/images/loading1.png"}}],
   },
   getters: {},
   mutations: {
