28256ed28947a69db08f2bd89c890115ea22d6b7
Shion Ichikawa <shion1305@gmail.com>
2022-10-21 11:24:17 +0900

    ✨ UnderlinedSectionTitle.vue

---
+++ src/components/utils/UnderlinedSectionTitle.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <h2>
+    <slot/>
+  </h2>
+</template>
+
+<style scoped>
+h2 {
+  font-weight: bolder;
+  font-size: 1.8em;
+  width: fit-content;
+  margin: 0.5em auto;
+}
+
+h2:after {
+  content: "";
+  float: left;
+  background: #b49b29;
+  width: 100%;
+  height: 6px;
+  border-radius: 3px;
+}
+</style>
