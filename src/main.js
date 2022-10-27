cff4ce07462b32ba2847c37d6116e972dc2b42aa
Shion Ichikawa <shion1305@gmail.com>
2022-10-27 18:28:17 +0900

    ✨ gtagの有効化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGtag from "vue-gtag";
 
-createApp(App).use(store).use(router).mount('#app')
+if (process.env.NODE_ENV === "production") {
+  createApp(App).use(store).use(router).use(VueGtag, {config: {id: 'G-KWMEVYXEHM'}}).mount('#app')
+} else {
+  createApp(App).use(store).use(router).mount('#app')
+}
