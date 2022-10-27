0cbf2ed82fa2725192df942288216a4d645ca778
Shion Ichikawa <shion1305@gmail.com>
2022-10-27 13:19:26 +0900

    ✨ HomeSearchSectionのジャンル検索機能

--- src/components/home/HomeSearchSection.vue
+++ src/components/home/HomeSearchSection.vue
@@ -2,10 +2,10 @@
   <div class="home-event-search-section">
     <StarTitle title="企画を探す" class="star-title"/>
     <div class="genre-buttons">
-      <router-link to="/events">展示</router-link>
-      <router-link to="/events">飲食</router-link>
-      <router-link to="/events">パフォーマンス</router-link>
-      <router-link to="/events">ゲーム・スポーツ</router-link>
+      <router-link to="/events?genre=1" class="exhibition">展示</router-link>
+      <router-link to="/events?genre=10" class="food">飲食</router-link>
+      <router-link to="/events?genre=2" class="performance">パフォーマンス</router-link>
+      <router-link to="/events?genre=3" class="game"><span>ゲーム・</span><span>スポーツ</span></router-link>
     </div>
   </div>
 </template>
@@ -16,6 +16,8 @@
   flex-direction: column;
   background: rgba(21, 21, 35, 0.7);
   border-radius: 2em;
+  height: fit-content;
+  padding: 2em 0;
 
   .star-title {
     margin: .5em auto;
@@ -31,7 +33,6 @@
     > a {
       text-decoration: none;
       color: white;
-      height: 2em;
       font-size: 1.2em;
       width: 40%;
       line-height: 2;
@@ -39,12 +40,34 @@
       background: #2c3e50;
       padding: 0.5em;
     }
+
+    .exhibition {
+      background: #b36305;
+    }
+
+    .food {
+      background: #0598d2;
+    }
+
+    .performance {
+      background: #e32017;
+    }
+
+    .game {
+      background: #00782a;
+    }
   }
 
-  .search-bar {
-
+  span {
+    display: inline-block;
   }
 
+  @media screen and (max-width: 350px) {
+    > a {
+      font-size: 1em;
+      padding: 0.2em
+    }
+  }
 }
 </style>
 <script>
